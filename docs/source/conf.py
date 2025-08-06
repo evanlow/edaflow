1f347daa06658a439a8ffa597a03612721b6aed0
Evan Low <evan.low@illumetechnology.com>
2025-08-06 16:31:40 +0800

    Update documentation version to v0.12.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ copyright = '2025, Evan Low'
 author = 'Evan Low'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.6'
-version = '0.8.6'
+release = '0.12.3'
+version = '0.12.3'
 
 
 # -- General configuration ---------------------------------------------------
